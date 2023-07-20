d412afe143434df06f4906f0dab76612fa9a7e7d
CozyBrian <dragonbrian33@gmail.com>
2023-07-20 04:20:43 +0000

    add: keyboard avoiding view

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,6 +1,7 @@
 import FillButton from "@/components/buttons/FillButton";
 import AuthHeader from "@/components/header/authHeader";
 import { Link } from "expo-router";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { Pressable, Text, TextInput, View } from "react-native";
 
 export default function SignIn() {
@@ -8,35 +9,40 @@ export default function SignIn() {
     <View className="bg-white flex-1">
       <AuthHeader />
       <View className="flex-1 items-center justify-center">
-        <View className="flex-col gap-y-[14px] items-center w-full px-9">
-          <Text
-            style={{ fontFamily: "Nunito_600SemiBold" }}
-            className="text-2xl pb-2"
-          >
-            Login to Your Account
-          </Text>
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor={"#667085"}
-            className="w-full h-11 border border-permuta-edge rounded-lg px-4"
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor={"#667085"}
-            className="w-full h-11 border border-permuta-edge rounded-lg px-4"
-          />
-          <View className="w-full pt-4">
-            <FillButton label="Sign In" className="" />
+        <KeyboardAvoidingView
+          className="w-full"
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View className="flex-col gap-y-[14px] items-center w-full px-9">
+            <Text
+              style={{ fontFamily: "Nunito_600SemiBold" }}
+              className="text-2xl pb-2"
+            >
+              Login to Your Account
+            </Text>
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor={"#667085"}
+              className="w-full h-11 border border-permuta-edge rounded-lg px-4"
+            />
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"#667085"}
+              className="w-full h-11 border border-permuta-edge rounded-lg px-4"
+            />
+            <View className="w-full pt-4">
+              <FillButton label="Sign In" className="" />
+            </View>
+            <Link href="/(auth)/sign-up" replace asChild>
+              <Pressable className="pt-2">
+                <Text>
+                  Don’t have an account?{" "}
+                  <Text className="font-bold">Sign Up</Text>
+                </Text>
+              </Pressable>
+            </Link>
           </View>
-          <Link href="/(auth)/sign-up" replace asChild>
-            <Pressable className="pt-2">
-              <Text>
-                Don’t have an account?{" "}
-                <Text className="font-bold">Sign Up</Text>
-              </Text>
-            </Pressable>
-          </Link>
-        </View>
+        </KeyboardAvoidingView>
       </View>
     </View>
   );
